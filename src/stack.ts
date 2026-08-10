046d33ff5be107d5cdb123824b9c1a67040ab6b9
Ore Ajibade <109004601+king525dev@users.noreply.github.com>
2026-08-10 19:52:42 +0100

    Create stack.ts

---
+++ src/stack.ts
@@ -0,0 +1,58 @@
+class Stack {
+    private data: Uint8Array;
+    private sp: number;   // stack pointer (0 = empty)
+
+    constructor(size = 256) {
+        this.data = new Uint8Array(size);
+        this.sp = 0;
+    }
+
+    push(value: any): void {
+        if (this.sp >= this.data.length) throw new Error("Stack overflow");
+        this.data[this.sp++] = value & 0xFF;
+    }
+
+    pop(): any{
+        if (this.sp === 0){
+            throw new Error("Stack underflow");
+        } else {
+            this.data[--this.sp];
+        }
+        return this.data[this.sp++]
+    }
+
+    peek(offset = 0): number|undefined {
+        return this.data[this.sp - 1 - offset];
+    }
+
+    dup(): void {
+        if (this.sp < 1) throw new Error("Stack underflow");
+        this.push(this.peek());
+    }
+
+    swap(): void {
+        const a = this.pop(), b = this.pop();
+        this.push(a); this.push(b);
+    }
+
+    nip(): void {
+        if (this.sp < 2) throw new Error("Not enough items");
+        const top = this.pop();
+        this.pop();          
+        this.push(top);
+    }
+
+    ovr(): void {        
+        if (this.sp < 2) throw new Error("Not enough items");
+        this.push(this.peek(1));
+    }
+
+    rot(): void {    
+        const c = this.pop(), b = this.pop(), a = this.pop();
+        this.push(b); this.push(c); this.push(a);
+    }
+
+    clr(): void {
+        this.sp = 0;
+    }
+}

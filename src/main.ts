85fecf4b0da35afc326c994446285416df4e32a5
Ore Ajibade <109004601+king525dev@users.noreply.github.com>
2026-08-29 03:23:40 +0100

    Successfully Tested VARS

--- src/main.ts
+++ src/main.ts
@@ -2,26 +2,14 @@ import Assembler  from "./assembler.js";
 import CPU from "./cpu/cpu.js";
 
 const source = `
-LDA 100
-LDA 108
-LDA 114
-LDA 111
-LDA 87
-LDA 111
-LDA 108
-LDA 108
-LDA 101
-LDA 104
-DCD 
-DCD
-DCD
-DCD
-DCD
-DCD 
-DCD
-DCD
-DCD
-DCD
+LDA 10
+STA 5
+LDA 15
+STA 6
+LDR 5
+LDR 6
+ADD
+OUT
 BRK
 `;
 

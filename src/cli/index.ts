7c1c9dbdddde2d37f15139afe616920c72b5d31c
Ore Ajibade <109004601+king525dev@users.noreply.github.com>
2026-09-22 01:17:13 +0100

    Update index.ts

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -214,7 +214,7 @@ function main(): number {
         logger.stop();
     });
 
-    logger.start("OXNTAL");
+    logger.start(`OXNTAL CLI @ ${ new Date().toLocaleString()}`);
     if (opts.logTarget.kind === "file") {
         logger.info(`Logging to ${opts.logTarget.path}`);
     }
